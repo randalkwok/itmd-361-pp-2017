800f5f61ec644548295a5ecc6407319ec69e0ed7
Randal <rkwok1@hawk.iit.edu>
2017-10-29 21:12:23 -0500

    Make anonymous function

--- pp-04/site.js
+++ pp-04/site.js
@@ -18,6 +18,10 @@ console.log('The value of x is:', x, 'It should be 5.');
 //  function can be accessed via the `window` global object,
 //  like `window.x`):
 
+(function double(num) {
+  var x = num * 2;
+  return x;
+})();
 
 //  3. Correct this function so that there is no i variable in
 //  the global scope:

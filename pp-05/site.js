dcb15d327436f05215a23b905102ad6d277e4237
Randal Kwok <rkwok1@otsads.iit.edu>
2017-11-13 14:15:28 -0600

    Add javacript button

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,4 +1,17 @@
-function ringDoorbell() {
-  var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
+$.noConflict();
+
+(function($) {
+  
+  $('#html5audio').remove();
+  $('#html-ring').remove();
+  $('body').append("<button>Ring the Doorbell</button>");
+
+  function ringDoorbell() {
+    var doorbell = new Audio('media/doorbell.mp3');
+    doorbell.play();
+  }
+  
+  $('button').on('click', function() {
+    ringDoorbell();
+  });
+})(jQuery);
